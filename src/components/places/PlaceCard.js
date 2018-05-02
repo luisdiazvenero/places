ea4ad83ab11ed47a80fc09bec0889be504eafc45
Luis <luisdiazvenero@gmail.com>
2018-05-02 10:48:51 -0500

    definiendo transiciones

--- src/components/places/PlaceCard.js
+++ src/components/places/PlaceCard.js
@@ -3,23 +3,34 @@ import React from 'react';
 import { Card, CardText, CardMedia, CardTitle, CardActions } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import {indigo400, redA400, lightBlueA400, amberA400} from 'material-ui/styles/colors';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 export default class PlaceCard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      show: true
+    }
+    setInterval(()=>this.setState({show: !this.state.show}),5000)
+  }
+
   render(){
     return(
-      <div className="col-xs-12 col-sm-4" key={this.props.index}>
-        <Card>
-          <CardMedia>
-            <img src={process.env.PUBLIC_URL + this.props.place.imageUrl}></img>
-          </CardMedia>
-          <CardTitle title={this.props.place.title}></CardTitle>
-          <CardText>{this.props.place.description}</CardText>
-          <CardActions style={{'textAlign': 'right'}}>
-            <FlatButton secondary={true} label="Ver más"></FlatButton>
-            <FlatButton secondary={true} onClick={()=>this.props.onRemove(this.props.place)} label="Ocultar"></FlatButton>
-          </CardActions>
-        </Card>
-      </div>
+      <CSSTransition classNames="fade-scale" in={this.state.show}>
+        <div className="col-xs-12 col-sm-4" key={this.props.index}>
+          <Card>
+            <CardMedia>
+              <img src={process.env.PUBLIC_URL + this.props.place.imageUrl}></img>
+            </CardMedia>
+            <CardTitle title={this.props.place.title}></CardTitle>
+            <CardText>{this.props.place.description}</CardText>
+            <CardActions style={{'textAlign': 'right'}}>
+              <FlatButton secondary={true} label="Ver más"></FlatButton>
+              <FlatButton secondary={true} onClick={()=>this.props.onRemove(this.props.place)} label="Ocultar"></FlatButton>
+            </CardActions>
+          </Card>
+        </div>
+      </CSSTransition>
     )
   }
 }
