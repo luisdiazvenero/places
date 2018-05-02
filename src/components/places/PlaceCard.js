26094c5e6e87d04d5c5dd4eb44db2b8f11700c92
Luis <luisdiazvenero@gmail.com>
2018-05-02 09:50:53 -0500

    actualizando vista con state, funcion "borrar" item

--- src/components/places/PlaceCard.js
+++ src/components/places/PlaceCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Card, CardText, CardMedia, CardTitle} from 'material-ui/Card';
-
+import { Card, CardText, CardMedia, CardTitle, CardActions } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import {indigo400, redA400, lightBlueA400, amberA400} from 'material-ui/styles/colors';
 
 export default class PlaceCard extends React.Component {
@@ -14,6 +14,10 @@ export default class PlaceCard extends React.Component {
           </CardMedia>
           <CardTitle title={this.props.place.title}></CardTitle>
           <CardText>{this.props.place.description}</CardText>
+          <CardActions style={{'textAlign': 'right'}}>
+            <FlatButton secondary={true} label="Ver más"></FlatButton>
+            <FlatButton secondary={true} onClick={()=>this.props.onRemove(this.props.place)} label="Ocultar"></FlatButton>
+          </CardActions>
         </Card>
       </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,11 +12,27 @@ import data from '../request/places';
 
 export default class Home extends React.Component{
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      places: data.places
+    }
+
+    this.hidePlace = this.hidePlace.bind(this);
+  }
+
   places(){
-    return data.places.map((place,index)=>{
+    return this.state.places.map((place,index)=>{
       return(
-        <PlaceCard place={place} index={index}></PlaceCard>
+        <PlaceCard onRemove={this.hidePlace} place={place} index={index}></PlaceCard>
       )
+    })
+  }
+
+  hidePlace(place){
+    this.setState({
+      places: this.state.places.filter(el => el != place)
     })
   }
 
